4f50b46a147fbb15311f99af9c271bc5dac16aa2
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-10-21 12:35:35 -0400

    Update qr, added codes

--- qRcode.py
+++ qRcode.py
@@ -1,5 +1,10 @@
 import qrcode
 
-img = qrcode.make("Welcome to my page!")
+# img = qrcode.make("Welcome to my page!")
 
-img.save("WelcomeQR.png")
+img = qrcode.make('https://github.com/dkimbi')
+imgGit = qrcode.make('https://github.com/')
+
+img.save("myPageQR.png")
+
+imgGit.save("gitHome.png")
